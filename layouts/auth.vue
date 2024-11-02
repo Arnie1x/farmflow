d5c851527c83b2b750416b2536b2200d569ce759
Arnie1x <arnie752@gmail.com>
2024-11-02 14:11:06 +0300

    feat(pages/auth/signin.vue): Created sign in UI and Layout Structure
    Next is adding CSS and aligning the image well

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <slot />
+  <div
+    class="w-screen h-screen flex flex-row justify-center items-center bg-gradient-to-br from-[#24252D] to-[#06240A] p-2">
+
+    <div class="h-full w-full">
+      <slot />
+    </div>
+
+    <div class="h-full w-full bg-white rounded-2xl">
+      <slot name="image" />
+    </div>
+
   </div>
 </template>
-
-<script lang="ts" setup>
-
-</script>
-
-<style>
-
-</style>
+<style scoped></style>
---
+++ pages/auth/signin.vue
@@ -0,0 +1,29 @@
+<template>
+  <NuxtLayout name="auth">
+    <div class="w-full h-full flex flex-col gap-3 justify-center items-center text-white">
+      <Logo />
+      <p class="text-3xl">Sign In</p>
+      <form class="inline-flex flex-col gap-3">
+        <input type="text" name="email" id="email" placeholder="Email">
+        <input type="password" name="password" id="password" placeholder="Password">
+        <p>Forgot your Password?</p>
+        <button type="submit">Sign In</button>
+      </form>
+      <p>Don't have an account? Sign Up</p>
+    </div>
+    <template #image>
+      <div class="w-full h-full">
+        <NuxtImg src="/images/sign-in.png" fit="fill" />
+      </div>
+    </template>
+  </NuxtLayout>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: false
+})
+
+</script>
+
+<style scoped></style>
