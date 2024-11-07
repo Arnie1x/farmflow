3b828b388b6dd87c0e0c2a31b4024427cc8d599d
Arnie1x <arnie752@gmail.com>
2024-11-07 20:49:05 +0300

    feat(pages/app/farms/[id].vue): Created the ID page for a farm
    Created all necessary components as well to make backend implementation easier.
    
    feat(layouts/app.vue): Enabled scrolling functionality using y-overflow

--- layouts/app.vue
+++ layouts/app.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="w-screen h-screen flex flex-row justify-between items-center bg-gradient-to-br from-[#1C1C1F] to-[#06240A] via-[#1D2122] via-30% p-2 gap-2">
+    class="w-screen h-screen flex flex-row justify-between items-center bg-gradient-to-br from-[#1C1C1F] to-[#06240A] via-[#1D2122] via-25% p-2 gap-2">
 
     <div class="w-full h-full max-w-[18.75rem]">
       <SideBar />
@@ -10,7 +10,7 @@
       <div class="w-full h-[4rem] flex flex-row justify-between items-center border-gray-300 border-b-2 p-5">
         <slot name="header" />
       </div>
-      <div class="p-5">
+      <div class="p-5 overflow-y-auto">
         <slot />
       </div>
     </div>
