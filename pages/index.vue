d0ff794db2aacce0f0262e00c16fb57b8758c10e
Arnie1x <arnie752@gmail.com>
2024-11-07 10:47:02 +0300

    feat(pages/index.vue): Added the Auth middleware

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
-  <h1>Homepage</h1>
+  <h1 class="p-5 text-xl">Homepage</h1>
 </template>
 
 <script lang="ts" setup>
-
+definePageMeta({
+  middleware: 'auth'
+})
 </script>
 
 <style></style>
