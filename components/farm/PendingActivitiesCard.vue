84c56f74d267bfcb51367974ab3f6c0f97881e04
Arnie1x <arnie752@gmail.com>
2024-11-12 19:29:20 +0300

    feat(components/farm/PendingActivitiesCard.vue): Pending Activities are now fetched from the Database

--- components/farm/PendingActivitiesCard.vue
+++ components/farm/PendingActivitiesCard.vue
@@ -4,17 +4,61 @@
       <NuxtImg src="/images/icons/alert.svg" alt="ai" class="w-[2.125rem]" />
       <h1 class="text-3xl font-bold text-[#D36135]">Pending Activities</h1>
     </div>
-    <div class="w-full h-full flex flex-col gap-5">
-      <p class="text-2xl font-bold">Month</p>
-      <FarmPendingActivityItem />
-      <FarmPendingActivityItem />
-      <FarmPendingActivityItem />
-      <FarmPendingActivityItem />
+    <div v-for="group in sortedGroupedEvents" :key="group.month" class="w-full h-full flex flex-col gap-5">
+      <p class="text-2xl font-bold">{{ group.month }}</p>
+      <div v-for="event in group.events" :key="event.id">
+        <FarmPendingActivityItem :event="event" />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
+const route = useRoute()
+const client = useSupabaseClient()
+
+const farmId = route.params.id
+const events = await getEvents()
+events?.reverse()
+
+const groupedEvents = events?.reduce((acc, event) => {
+  const month = new Date(event.end_date).toLocaleString('default', { month: 'long' });
+  if (!acc[month]) {
+    acc[month] = [];
+  }
+  acc[month].push(event);
+  return acc;
+}, {});
+
+const sortedGroupedEvents = Object.keys(groupedEvents).sort((a, b) => {
+  const dateA = new Date(a);
+  const dateB = new Date(b);
+  return dateA - dateB;
+}).map(key => ({ month: key, events: groupedEvents[key] }));
+
+// console.log(sortedGroupedEvents);
+
+async function getEvents() {
+  try {
+    const today = new Date();
+    const threeDaysFromNow = new Date(today.getTime() + 3 * 24 * 60 * 60 * 1000).toISOString();
+
+    let { data: events, error } = await client
+      .from('events')
+      .select('*')
+      .eq('farm_id', farmId)
+      .lte('end_date', threeDaysFromNow)
+      .order('end_date', { ascending: false })
+      .limit(5)
+
+    if (error) {
+      throw error
+    }
+    return events
+  } catch (error) {
+    console.log(error)
+  }
+}
 
 </script>
 
--- components/farm/PendingActivityItem.vue
+++ components/farm/PendingActivityItem.vue
@@ -1,18 +1,36 @@
 <template>
-  <div class="w-full flex flex-row justify-between items-center">
-    <div class="flex flex-col justify-center items-center px-8 pr-14">
-      <p class="text-4xl font-bold text-[#D36135]">21</p>
-      <p class="text-[#848AA0]">09:00</p>
+  <NuxtLink :to="{ name: 'app-farms-id-events-event_id', params: { id: event.farm_id, event_id: event.id } }">
+    <div class="w-full flex flex-row justify-between items-center">
+      <div class="flex flex-col justify-center items-center px-8 pr-14 min-w-[10rem]">
+        <p class="text-4xl font-bold text-[#D36135]">{{ getDayDate(event.end_date) }}</p>
+        <p class="text-[#848AA0]">{{ getTimeDate(event.end_date) }}</p>
+      </div>
+      <div class="h-[4rem] border border-gray-400" />
+      <div class="flex w-full items-center justify-center">
+        <p class="text-2xl text-ellipsis p-3">{{ event.name }}</p>
+      </div>
     </div>
-    <div class="h-full border border-gray-400" />
-    <div class="flex w-full items-center justify-center">
-      <p class="text-2xl text-ellipsis">Planting Seedlings</p>
-    </div>
-  </div>
+  </NuxtLink>
 </template>
 
 <script lang="ts" setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
 
+function getDayDate(date) {
+  const day = new Date(date).toLocaleString('default', { day: 'numeric' });
+  return day
+}
+
+function getTimeDate(date) {
+  const time = new Date(date).toLocaleString('default', { hour: 'numeric', minute: 'numeric' });
+  console.log(time)
+  return time
+}
 </script>
 
 <style></style>
