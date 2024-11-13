b7e47181fdbc84922a160a8c99c1a54e09b2d0a3
Arnie1x <arnie752@gmail.com>
2024-11-13 17:55:29 +0300

    feat(pages/app/calendar/Item.vue): Event data that is displayed is now fetched from the database

--- components/calendar/Item.vue
+++ components/calendar/Item.vue
@@ -1,34 +1,33 @@
 <template>
   <div class="w-full flex xl:flex-row flex-col justify-between items-center p-3 rounded-2xl border-2">
-    <div class="flex flex-row justify-center gap-5 items-center w-full xl:py-0 py-3">
-      <div class="flex flex-col justify-center items-center px-3 pl-8">
-        <p class="font-bold">Mon</p>
-        <p class="text-3xl font-bold text-[#2B9B3C]">28</p>
+    <div class="flex flex-row justify-start gap-5 items-center xl:py-0 py-3 w-fit">
+      <div class="flex flex-col justify-center items-center px-3 pl-8 w-fit">
+        <p class="font-bold">{{ getWeekDay(event.end_date) }}</p>
+        <p class="text-3xl font-bold text-[#2B9B3C]">{{ getDayDate(event.end_date) }}</p>
       </div>
       <div class="border h-[2.5rem] mx-4" />
-      <div class="flex flex-col min-w-[10rem]">
+      <div class="flex flex-col min-w-[10rem] justify-start">
         <div class="flex flex-row gap-2 justify-start">
           <NuxtImg src="/images/icons/location.svg" alt="location" class="w-[1.25rem]" />
-          <p class="text-base text-ellipsis line-clamp-1">Sunny Fields</p>
+          <p class="text-base text-ellipsis line-clamp-1">{{ event.farm_name }}</p>
         </div>
         <div class="flex flex-row gap-2 justify-start">
-          <NuxtImg src="/images/icons/location.svg" alt="location" class="w-[1.25rem]" />
-          <p class="text-base">09:00</p>
+          <NuxtImg src="/images/icons/time.svg" alt="location" class="w-[1.25rem]" />
+          <p class="text-base">{{ getTimeDate(event.end_date) }}</p>
         </div>
       </div>
     </div>
     <div class="border h-[2.5rem] mx-4 xl:block hidden" />
-    <div>
-      <p class="text-ellipsis line-clamp-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore impedit
-        doloribus dolorem numquam similique vitae, illo dolores. Amet sunt blanditiis rem pariatur eum unde incidunt
-        consectetur praesentium! Id repellat deleniti vitae aliquid, repellendus exercitationem delectus dolor
-        fugiat
-        qui quos consequuntur iure dolorem numquam minus aut sequi mollitia magni. Ea nihil sint mollitia eligendi
-        laborum!</p>
+    <div class="w-full">
+      <p class="text-ellipsis line-clamp-2 w-full">
+        <b>{{ event.name }}: </b> <br>
+        {{ event.description }}
+      </p>
     </div>
     <div>
-      <!-- <NuxtLink :to="{ name: 'app-farms-id-events-event_id', params: { id: farmId, event_id: event.id } }" -->
-      <NuxtLink class="link flex flex-row gap-3 justify-center items-center p-4 min-w-[8rem]">
+      <!-- <NuxtLink class="link flex flex-row gap-3 justify-center items-center p-4 min-w-[8rem]"> -->
+      <NuxtLink :to="{ name: 'app-farms-id-events-event_id', params: { id: event.farm_id, event_id: event.id } }"
+        class="link flex flex-row gap-3 justify-center items-center p-4 min-w-[8rem]">
         <p class="text-xl">Edit</p>
         <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M17.5 5.25L10 12.75L2.5 5.25L1 6.75L10 15.75L19 6.75L17.5 5.25Z" fill="#104547" class="link-svg" />
@@ -39,11 +38,29 @@
 </template>
 
 <script lang="ts" setup>
+defineProps({
+  event: {
+    type: Object,
+    required: true
+  }
+})
+function getDayDate(date) {
+  const day = new Date(date).toLocaleString('default', { day: 'numeric' });
+  return day
+}
+function getWeekDay(date) {
+  const day = new Date(date).toLocaleString('default', { weekday: 'short' });
+  return day
+}
+
+function getTimeDate(date) {
+  const time = new Date(date).toLocaleString('default', { hour: 'numeric', minute: 'numeric' });
+  return time
+}
 
 </script>
 
 <style scoped>
-
 .link-svg {
   fill: #104547;
   transition: 0.5s all ease;
@@ -63,5 +80,4 @@
 .link:hover .link-svg {
   fill: #2ac241;
 }
-
 </style>
--- pages/app/calendar/index.vue
+++ pages/app/calendar/index.vue
@@ -3,9 +3,11 @@
     <template #header>
       <h1 class="text-3xl font-bold">Calendar</h1>
     </template>
-    <div class="flex flex-col gap-3">
-      <p class="text-2xl font-bold">November</p>
-      <CalendarItem />
+    <div v-for="group in sortedGroupedEvents" :key="group.month" class="flex flex-col gap-3">
+      <p class="text-2xl font-bold">{{ group.month }}</p>
+      <div v-for="event in group.events" :key="event.id">
+        <CalendarItem :event="event" />
+      </div>
     </div>
   </NuxtLayout>
 </template>
@@ -14,6 +16,60 @@
 definePageMeta({
   middleware: 'auth'
 })
+
+const client = useSupabaseClient()
+const events = await getEvents()
+
+for (const event of events) {
+  event.farm_name = await getFarmName(event.farm_id)
+}
+
+const groupedEvents = events?.reduce((acc, event) => {
+  const month = new Date(event.end_date).toLocaleString('default', { month: 'long' });
+  if (!acc[month]) {
+    acc[month] = [];
+  }
+  acc[month].push(event);
+  return acc;
+}, {});
+
+const sortedGroupedEvents = Object.keys(groupedEvents).sort((a, b) => {
+  const dateA = new Date(a);
+  const dateB = new Date(b);
+  return dateA - dateB;
+}).map(key => ({ month: key, events: groupedEvents[key] }));
+
+async function getEvents() {
+  try {
+    let { data: events, error } = await client
+      .from('events')
+      .select('*')
+      .order('end_date', { ascending: true })
+      .eq('is_completed', false)
+    if (error) {
+      throw error
+    }
+    return events
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function getFarmName(farmId) {
+  try {
+    let { data: farm, error } = await client
+      .from('farms')
+      .select('name')
+      .eq('id', farmId)
+      .single()
+    if (error) {
+      throw error
+    }
+    return farm.name
+  } catch (error) {
+    console.log(error)
+}
+}
 </script>
 
 <style scoped>
