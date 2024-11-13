479dba17640f9f26e9fc5166ebeec56bdba662fa
Arnie1x <arnie752@gmail.com>
2024-11-13 17:14:38 +0300

    feat(pages/app/calendar/index.vue): Created the front-end for the calendar page including the CalendarItem component

--- pages/app/calendar/index.vue
+++ pages/app/calendar/index.vue
@@ -3,6 +3,10 @@
     <template #header>
       <h1 class="text-3xl font-bold">Calendar</h1>
     </template>
+    <div class="flex flex-col gap-3">
+      <p class="text-2xl font-bold">November</p>
+      <CalendarItem />
+    </div>
   </NuxtLayout>
 </template>
 
