ad3e9caecfe909889ad9a4ebfd48ff5eade9f315
Arnie1x <arnie752@gmail.com>
2024-11-18 23:25:49 +0300

    feat(pages/app/faq/index.vue): Created the FAQ Page with simple placeholder data

---
+++ components/faq/FaqItem.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex flex-col gap-3 w-full border-2 rounded-2xl p-5 pb-2 cursor-pointer" @click="isOpen = !isOpen">
+    <p class="text-2xl font-bold">{{ question}}</p>
+    <div v-auto-animate>
+      <p v-if="isOpen" class="whitespace-pre-line">{{ answer }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+  answer: {
+    type: String,
+    required: false,
+    default: ''
+  },
+  question: {
+    type: String,
+    required: false,
+    default: ''
+  }
+})
+
+const isOpen = ref(false)
+</script>
+
+<style>
+
+</style>
--- pages/app/faq/index.vue
+++ pages/app/faq/index.vue
@@ -3,12 +3,55 @@
     <template #header>
       <h1 class="text-3xl font-bold">FAQ</h1>
     </template>
+    <div class="h-full w-full max-w-[64rem] mx-auto flex flex-col gap-3">
+      <FaqItem v-for="item in data" :question="item.question" :answer="item.answer" />
+    </div>
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   middleware: 'auth'
+})
+
+const data = ref([])
+
+data.value.push({
+  question: 'How do I start interacting with the chatbot?',
+  answer: `
+  To interact with the chatbot:
+
+- Navigate to the "Chat" section from the main menu.
+- Type your question or request in the input box at the bottom of the chat page.
+- The chatbot will provide a response tailored to your query.
+- You can also reference specific farms using the @ symbol to include farm-specific context.
+
+The chatbot is designed to provide clear, concise answers and guide you step-by-step for your rice farming needs.
+
+`
+})
+data.value.push({
+  question: 'How can I link my farm details to the chatbot?',
+  answer: `
+  You can link your farm details to the chatbot by referencing your farm's name in your message. Here’s how:
+
+- While chatting, type the @ symbol followed by your farm's name.
+- The chatbot will use the associated farm data (e.g., soil type, location) to give tailored advice.
+- Make sure your farm profile is up-to-date under the "Farm Profile" section in the app!
+
+`
+})
+data.value.push({
+  question: 'How can I access and update my farm profile?',
+  answer: `
+To manage your farm profile:
+
+- Go to the "Farms" page from the navigation menu.
+- Add new farms by clicking "Add Farm" and entering the required details, such as farm size, soil type, and location.
+- To update an existing farm, select the farm from the list, make your changes, and save them.
+- Your farm profile helps the app and chatbot provide personalized recommendations.
+
+`
 })
 </script>
 
